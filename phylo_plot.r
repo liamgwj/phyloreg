f9a5584ffb51b8db226709366b6131d462aa5a2c
Liam Johnson <liam.gwjohnson@gmail.com>
2022-03-11 13:51:01 -0800

    added dashed lines to phylo plots

--- phylo_plot.r
+++ phylo_plot.r
@@ -1,47 +1,18 @@
 # LJ 2022-03-09 plot model predictions presentably on phylogeny
 
-# add line type - dashed lines for tips not used in model fitting
+# something's still up - all sisters in a polytomy should be same p/colour
 
 # setup -----------------------------------------------------------------------
 
 library(ape)
 library(viridis)
 
-setwd("/home/liam/Documents/MSc/analysis/phyloreg/LJ_Parker")
-
-# read in phylogeny
-# modified QJ/Z (missing 2 genera/tips)
-phy <- read.tree("output/QJ_Parker_2022-03-09.nwk")
-
-# Parker original, with my additions (many polytomies)
-#phy <- read.tree("output/cleaned_Parker_phylogeny_2022-02-28.nwk")
-
-
-# read in model coefficients
-coef_1 <- read.csv("output/model_coefficients/parker-QJ-all_coef-av_2022-03-09T12-31-19.csv")
-coef_2 <- read.csv("output/model_coefficients/parker-QJ-halfhosts_coef-av_2022-03-10T15-38-24.csv")
-
-
-# read in host/pest db
-db <- read.csv("output/cleaned_Parker_database_2022-02-28.csv",
-               row.names = 1)
-
 
 # generate tip-level predictions for plotting ---------------------------------
 
-# format list of coefficients
-coef_lst <- vector("list", length = nrow(coef_2))
-
-names(coef_lst) <- coef_2$Pest
-
-for(i in 1:nrow(coef_2)){
-coef_lst[[i]] <- rbind(coef_1[which(coef_1$Pest == coef_2$Pest[i]) ,],
-                       coef_2[i,])
-}
-
 # choose a pest
-i=1
-fpest <- coef_lst[[i]]$Pest[1]
+j=2
+fpest <- coef_lst[[j]]$Pest[1]
 
 fp_df <- data.frame(t(db[fpest,]))
 colnames(fp_df) <- "compatible"
@@ -62,16 +33,15 @@ fp_df$minPD <- apply(fp_df[,(ncol(fp_df)-length(hosts)+1):ncol(fp_df)],1,min)
 
 
 
-fp_df$logit1min <- coef_lst[[i]]$intercept[1] + coef_lst[[i]]$slope[1] * log10(fp_df$minPD + 1)
+fp_df$logit1min <- coef_lst[[j]]$intercept[1] + coef_lst[[j]]$slope[1] * log10(fp_df$minPD + 1)
 
 fp_df$pred1min <- exp(fp_df$logit1min)/(1 + exp(fp_df$logit1min))
 
 # if predicted value was too big, division by Inf has given NA - convert to 1s
 fp_df$pred1min <- ifelse(is.na(fp_df$pred1min), 1, fp_df$pred1min)
 
-i=1
 
-fp_df$logit2min <- coef_lst[[i]]$intercept[2] + coef_lst[[i]]$slope[2] * log10(fp_df$minPD + 1)
+fp_df$logit2min <- coef_lst[[j]]$intercept[2] + coef_lst[[j]]$slope[2] * log10(fp_df$minPD + 1)
 
 fp_df$pred2min <- exp(fp_df$logit2min)/(1 + exp(fp_df$logit2min))
 
@@ -108,21 +78,36 @@ edgecols2[which.edge(phy, hosts[i])] <- "blue"
 }
 
 
+# line types
+mod2tips <- unique(coef_2a$FocalHost)
+mod2notinc <- setdiff(phy$tip.label, mod2tips)
+
+
+edgelines <- rep(1, Nedge(phy))
+
+for(i in seq_along(mod2notinc)){
+edgelines[which.edge(phy, mod2notinc[i])] <- 2
+}
+
+
 
 # plot ------------------------------------------------------------------------
 
-depth <- max(node.depth.edgelength(phy))
+#depth <- max(node.depth.edgelength(phy))
 
 par(mfrow=c(1,2))
 plot(phy,
-     x.lim = depth+(depth/10),
-     label.offset = 5,
+     show.tip.label = FALSE,
+#     x.lim = depth-(depth/9),
+#     label.offset = 5,
      no.margin = TRUE,
      edge.color = edgecols1)
 plot(phy,
      direction = "leftwards",
+     no.margin = TRUE,
      show.tip.label = FALSE,
-     edge.color = edgecols2)
+     edge.color = edgecols2,
+     edge.lty = edgelines)
 
 
 
--- plotting_UI.r
+++ plotting_UI.r
@@ -1,6 +1,8 @@
 # LJ 2022-03-07 set up for plotting and source plotting script
 ## to be used interactively!
 
+library(ape)
+
 # choose dataset/model coefficient files to plot -------------------------------
 
 # Parker 2015 -------------------
@@ -8,8 +10,14 @@
 setwd("/home/liam/Documents/MSc/analysis/phyloreg/LJ_Parker")
 
 coef_1 <- read.csv("output/model_coefficients/parker-QJ-all_coef-av_2022-03-09T12-31-19.csv")
-coef_2 <- read.csv("output/model_coefficients/parker-QJ-half_coef-av_2022-03-09T12-33-37.csv")
+coef_2 <- read.csv("output/model_coefficients/parker-QJ-halfhosts_coef-av_2022-03-10T15-38-24.csv")
 
+# for phylo
+coef_2a <- read.csv("output/model_coefficients/parker-QJ-halfhosts_coef_2022-03-10T15-38-24.csv")
+phy <- read.tree("output/QJ_Parker_2022-03-09.nwk")
+#phy <- read.tree("output/cleaned_Parker_phylogeny_2022-02-28.nwk")
+db <- read.csv("output/cleaned_Parker_database_2022-02-28.csv",
+               row.names = 1)
 
 # Robles 2017 -------------------
 
@@ -17,6 +25,13 @@ setwd("/home/liam/Documents/MSc/analysis/phyloreg/LJ_Robles")
 
 coef_1 <- read.csv("output/model_coefficients/robles-all_coef-av_2022-03-08T09-57-30.csv")
 coef_2 <- read.csv("output/model_coefficients/robles-half_coef-av_2022-03-08T09-58-43.csv")
+
+coef_2a <- read.csv("output/model_coefficients/robles-half_coef_2022-03-08T09-58-43.csv")
+
+phy <- read.tree("indata/host.tree")
+
+db <- read.csv("output/cleaned_Robles_database_2022-03-07.csv",
+               row.names = 1)
 
 
 # format list of coefficients --------------------------------------------------
@@ -30,8 +45,12 @@ coef_lst[[i]] <- rbind(coef_1[which(coef_1$Pest == coef_2$Pest[i]) ,],
                        coef_2[i,])
 }
 
+# phylogeny plotting ----------------------------------------------------------
 
-# source plotting script -------------------------------------------------------
+
+
+
+# curve plotting ---------------------------------------------------------------
 
 # set ID for file naming
 ID <- "parker-QJ-ah"
